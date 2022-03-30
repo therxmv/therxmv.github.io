08f46b66b8f8be649abdff92b31dec82116c52aa
therxmv <roma.kyzmuch@gmail.com>
2022-03-30 13:32:35 +0300

    Reworked code and optimizate

--- FormulaOne-Timer/js/script.js
+++ FormulaOne-Timer/js/script.js
@@ -1,78 +1,86 @@
 //--API--//
 class F1Service {
-    getInfo = async (id) => {
-        const res = await fetch(`http://ergast.com/api/f1/2022/${id}.json`, {method: 'GET', redirect: 'follow'});
+    getInfo = async () => {
+        const res = await fetch(`http://ergast.com/api/f1/2022.json`, {method: 'GET', redirect: 'follow'});
         return await res.json();
     }
 
-    getDT = async (id) => {
-        const dt = await this.getInfo(id);
+    getAllRaces = async () => {
+        const races = await this.getInfo();
         return {
-            date: dt.MRData.RaceTable.Races[0].date,
-            time: dt.MRData.RaceTable.Races[0].time,
-            raceName: dt.MRData.RaceTable.Races[0].raceName,
+            races: races.MRData.RaceTable.Races
         }
     }
 }
 //--API--//
 
-const f1 = new F1Service();
+async function main() {
+    const f1 = new F1Service();
 
-const d = document.querySelector('#days'),
-    h = document.querySelector('#hours'),
-    m = document.querySelector('#min'),
-    s = document.querySelector('#sec'),
-    gpName = document.querySelector('#racename'),
-    //--START TIMER--//
-    timer = setInterval(updateTimer, 1000);
+    const d = document.querySelector('#days'),
+        h = document.querySelector('#hours'),
+        m = document.querySelector('#min'),
+        s = document.querySelector('#sec'),
+        gpName = document.querySelector('#racename'),
+        {races} = await f1.getAllRaces();
+    
+    async function updateTimer() {
+        const {days, hours, minutes, seconds, raceName} = getData();
 
-let remainTime, id = 1;
-
-async function updateTimer() {
-    const {days, hours, minutes, seconds, raceName} = await getRemainingTime();
-
-    if(seconds > 0) {
         document.querySelectorAll('.shimmerBG').forEach(elem => {
             elem.classList.remove('shimmerBG');
         })
 
-        d.textContent = addZero(days);
-        h.textContent = addZero(hours);
-        m.textContent = addZero(minutes);
-        s.textContent = addZero(seconds);
-        gpName.textContent = `To ${raceName} remain`;
+        d.textContent = days;
+        h.textContent = hours;
+        m.textContent = minutes;
+        s.textContent = seconds;
+        gpName.textContent = raceName;
+    }
+
+    const timer = setInterval(updateTimer, 1000);
+    
+    function getData() {
+        for(let i = 0; i < races.length; i++) {
+            const {date, time, raceName} = races[i];
+            
+            const deadline = new Date(`${date}T${time}`),
+                lastDeadline = new Date(`${races[races.length - 1].date}T${races[races.length - 1].time}`);
+
+            if(Date.parse(deadline) > Date.parse(new Date())) {
+                const remainTime = Date.parse(deadline) - Date.parse(new Date());
+
+                return {
+                    days: addZero(Math.floor(remainTime / (1000 * 60 * 60 * 24))),
+                    hours: addZero(Math.floor((remainTime / (1000 * 60 * 60)) % 24)),
+                    minutes: addZero(Math.floor((remainTime / (1000 * 60)) % 60)),
+                    seconds: addZero(Math.floor((remainTime / 1000) % 60)),
+                    raceName: `To ${raceName} remain`,
+                }
+            }
+            else if (Date.parse(lastDeadline) <= Date.parse(new Date())){
+                clearInterval(timer);
+
+                return {
+                    days: "00",
+                    hours: "00",
+                    minutes: "00",
+                    seconds: "00",
+                    raceName: `The end of season`,
+                }
+            }
+        }
+    }
+
+    // Add zero like: 8 -> 08
+    function addZero(num) {
+        if(num < 10){
+            return `0${num}`;
+        }
+        else {
+            return num;
+        }
     }
 }
 
-async function getRemainingTime() {
-    if(id > 22){
-        id = 1;
-    }
-
-    const {date, time, raceName} = await f1.getDT(id);
-    const deadline = new Date(`${date}T${time}`);
-
-    remainTime = Date.parse(deadline) - Date.parse(new Date());
-
-    if(remainTime <= 0){
-        id++;
-    }
-
-    return {
-        days: Math.floor(remainTime / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((remainTime / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((remainTime / (1000 * 60)) % 60),
-        seconds: Math.floor((remainTime / 1000) % 60),
-        raceName: raceName,
-    }
-}
-
-// Add zero like: 8 -> 08
-function addZero(num) {
-    if(num < 10){
-        return `0${num}`;
-    }
-    else {
-        return num;
-    }
-}
+main();
