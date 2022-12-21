cfcd8a07c4793458fb1783c4fda5edfc3ca811f7
therxmv <roma.kyzmuch@gmail.com>
2022-12-21 18:11:20 +0200

    Update parser

--- F1-api/script.js
+++ F1-api/script.js
@@ -1,5 +1,12 @@
 getAllSchedule = async () => {
-    const res = await fetch(`https://www.formula1.com/en/racing/${new Date().getFullYear()}.html`, { method: "GET", mode: 'cors'});
+    const res = await fetch(`https://www.formula1.com/en/racing/${new Date().getFullYear()}.html`, { 
+        method: "GET", 
+        mode: 'cors', 
+        headers: {
+            host: 'www.formula1.com',
+            accept: '*/*',
+        },
+    });
     return await res.text();
 };
 
@@ -18,7 +25,7 @@ async function main() {
     for(elem of hrefs) {
         const raceInfo = await getRaceInfo(elem);
         const raceEvents = parser.parseFromString(raceInfo, "text/html").getElementsByClassName("f1-race-hub--timetable-listings");
-        raceEvents[0].classList.add(elem.match(RegExp(/[^\/]+.\w+$/gm))[0].replace(".html", ""));//"[^\\/]+.\\w+\$".toRegex().find(url)!!.value.dropLast(5));
+        raceEvents[0].classList.add(elem.match(RegExp(/[^\/]+.\w+$/gm))[0].replace(".html", ""));
         document.body.appendChild(raceEvents[0]);
     }
 }
